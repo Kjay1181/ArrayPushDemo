0dfca15abb739479bdef4f512ed1f4413ed064a8
Kjay1181 <42454194+Kjay1181@users.noreply.github.com>
2018-08-16 13:42:42 -1000

    First Commit
    This is my first commit.

---
+++ arrayPushDemo.js
@@ -0,0 +1,11 @@
+var firstName=[];
+firstName.push(userFirst);
+
+var lastName=[];
+lastName.push(userLast);
+
+var userFirst=prompt("what is your first name?");
+
+var userLast= prompt("what is your last name?");
+
+window.alert(userFirst+userLast +userFirst.length + userLast.length);
