c14b1e6dda77dabdfa944053c858da4641e9f825
Kjay1181 <42454194+Kjay1181@users.noreply.github.com>
2018-08-16 13:47:30 -1000

    Update
    Made proper code

--- arrayPushDemo.js
+++ arrayPushDemo.js
@@ -2,11 +2,11 @@ var firstName=[];// start with empty array
 
 
 var lastName=[];// start with empty array
-firstName.push(userFirst);
-lastName.push(userLast);
+
+
 
 var userFirst=prompt("what is your first name?");
-
+firstName.push(userFirst);
 var userLast= prompt("what is your last name?");
-
+lastName.push(userLast);
 window.alert(userFirst+userLast +userFirst.length + userLast.length);
